f67c9ab1e1bda5b96e0872a9adb6b447ff6a3213
Justine L. Llamera <jllamera06@gmail.com>
2026-01-15 18:55:57 +0800

    Basic auth context

---
+++ app/(auth)/signIn.tsx
@@ -0,0 +1,15 @@
+import {View, Text, Dimensions} from "react-native"; 
+
+
+export default function SignUp () {
+    return (
+        <>
+        <View style={{flex: 1, backgroundColor: '#F2f2f2'}}>
+            <View>
+                <Text>Lorem ipsum dolor sit amet.</Text>
+            </View>
+        </View>
+        </>
+    )
+
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
@@ -12,7 +12,9 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  // const router = useRouter();
   const colorScheme = useColorScheme();
+  const userNotVerified = false;
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -23,6 +25,13 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
+  // User not verified redirect to layout
+  // useEffect(() => {
+  //   if (!userNotVerified) {
+  //     router.push('/(auth)/signIn');
+  //   }
+  // }, [])
+
   if (!loaded) {
     return null;
   }
@@ -30,7 +39,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
