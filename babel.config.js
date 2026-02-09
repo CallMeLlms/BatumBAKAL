28dcbca859b0d11189593637a2e85a2c2f1cd8a5
Justine L. Llamera <jllamera06@gmail.com>
2026-02-09 22:55:24 +0800

    JWT Implementation part 1

--- babel.config.js
+++ babel.config.js
@@ -7,8 +7,8 @@ module.exports = function (api) {
     ],
     plugins: [
       // Add any Babel plugins here if needed
-      'react-native-reanimated/plugin',
       'react-native-worklets/plugin',
+      'react-native-reanimated/plugin',
     ],
   };
 };
