53970f14fc62969117f86d810c9c2ea85ca90f6f
Justine L. Llamera <jllamera06@gmail.com>
2026-02-16 07:50:14 +0800

    Client ts, added middleware interceptors

--- api/client.ts
+++ api/client.ts
@@ -19,14 +19,39 @@ const apiClient = axios.create({
     }
 })
 
+let isRefreshing = false;
+const requestQueue = [];
+
+apiClient.interceptors.response.use (
+    async (response) => response,
+    async (error) => {
+        if (error.response?.status === 401) {
+            if (!isRefreshing) {
+                isRefreshing = true;
+            }
+        }
+
+        try {
+            const refreshToken = await get_jwt_token();
+            const res  = await fetch('/auth/refresh', 
+                {method: 'POST', body: JSON.stringify({refreshToken})}
+            );
+        } catch (error) {
+            
+        }
+    },
+)
+
+
 apiClient.interceptors.request.use(
-    async (config) => {
+    async (config)  => {
         const TOKEN = await get_jwt_token();
         if (TOKEN) {
             config.headers.Authorization = `Bearer ${TOKEN}`
         }
         return config;
-    }
+    } 
 )
+
 
 export default apiClient;
